28694db1d2acc1553d56f35a2fab4f3cc0025c3a
Arjun Tambe <arjuntambe141010@gmail.com>
2025-02-18 15:08:37 -0600

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArjunTeleOP.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ArjunTeleOP extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
